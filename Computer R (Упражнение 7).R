9eb8d5cd819a3ea53ca9a228381af83c89e16cc3
NorthCL <72108480+NorthCL@users.noreply.github.com>
2020-10-16 18:29:04 +0300

    Update Computer R (Упражнение 7).R

--- Computer R (Упражнение 7).R
+++ Computer R (Упражнение 7).R
@@ -1,13 +1,13 @@
-png("plot-ex07.png", width = 600)
+png("plot-ex071.png", width = 600)
 dfRegions <-  read.csv2("regions.csv", stringsAsFactors = T)
 dfRegions
-x <- c(dfRegions$year_2005)
-d <- c(dfRegions$year_2009)
-y <- c(dfRegions$year_2013)
-z <- c(dfRegions$year_2017)
-XYZ <- cbind(x/sum(x),d/sum(d), y/sum(y), z/sum(z))
-colnames(XYZD) <- c("2005","2009", "2013", "2017")
-rownames(XYZD) <- dfRegions$финансы
-barplot(XYZD, main = "Нижегородская область", beside = F,col = cm.colors(3,alpha = 1))
-legend("topright", legend = rownames(XYZ), fill = cm.colors(3, alpha = 1))
+# x <- c(dfRegions$year_2005)
+# d <- c(dfRegions$year_2009)
+# y <- c(dfRegions$year_2013)
+# z <- c(dfRegions$year_2017)
+# XYZ <- cbind(x/sum(x),d/sum(d), y/sum(y), z/sum(z))
+# colnames(XYZD) <- c("2005","2009", "2013", "2017")
+rownames(dfRegions) <- dfRegions$финансы
+barplot(as.matrix(dfRegions[,2:5]), main = "Нижегородская область", beside = F,col = cm.colors(3,alpha = 1))
+legend("topleft", legend = rownames(dfRegions), fill = cm.colors(3, alpha = 1))
 dev.off()
